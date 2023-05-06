945060ed8fbca07e642441d7e12acbaea1545d71
Shadowman405 <maximus405@gmail.com>
2023-05-06 03:44:40 +0300

    network task

---
+++ iTunes_API_test/ResultsTableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ResultsTableViewController.swift
+//  iTunes_API_test
+//
+//  Created by Maxim Mitin on 6.05.23.
+//
+
+import UIKit
+
+class ResultsTableViewController: UITableViewController {
+    
+    private var searchResults = [Welcome]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        callAPI()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if searchResults.count != 0 {
+            searchResults.count
+        } else {
+            return 1
+        }
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension ResultsTableViewController {
+    func callAPI() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson.") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            let decoder = JSONDecoder()
+            
+            if let data = data {
+                do {
+                    let results = try decoder.decode([Welcome].self, from: data)
+                    for result in results {
+                        self.searchResults.append(result)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        task.resume()
+        
+    }
+}
